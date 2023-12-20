d32c1839d3f6851f94a0fefd826551eff3f74737
sarah mitchell <g00391704@atu.ie>
2023-12-20 17:55:26 +0000

    Added verification for login

--- frontend/components/login/Login.js
+++ frontend/components/login/Login.js
@@ -1,4 +1,4 @@
-import {useRef} from "react";
+import {useRef, useState} from "react";
 
 import classes from '../books/NewBookForm.module.css';
 import Card from "../ui/Card";
@@ -22,17 +22,81 @@ function Login(props) {
         props.onAddUser(UserData);
     }
 
+    const [input , setInput] = useState({
+        email:'',
+        password: ''
+    })
+
+    const [error , setError] = useState({
+        email:'',
+        password: ''
+    })
+
+    const onInputChange = e => {
+        const {name, value} = e.target;
+        setInput(prev => ({
+            ...prev,
+            [name]: value
+        }));
+        validateInput(e);
+    }
+
+    const validateInput = e => {
+        let {name, value} = e.target;
+        setError(prev => {
+            const stateObj = {...prev, [name]: ""};
+
+            switch (name){
+                case "email":
+                    if(!value){
+                        stateObj[name] = "Please enter a valid student email";
+                    }
+                    break;
+
+                case "password":
+                    if(!value){
+                        stateObj[name] = "Please enter a password";
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return stateObj;
+        })
+    }
+
     return (
         <Card>
             <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor='email'>Email:</label>
-                    <input type='text' id ='email' required ref={emailInputRef}></input>
+                    <input
+                        type='text'
+                        id ='email'
+                        name = 'email'
+                        value={input.email}
+                        onChange={onInputChange}
+                        onBlur={validateInput}
+                        placeholder='Enter your student Email...'
+                        required
+                        ref={emailInputRef}></input>
+                    {error.email && <span className={classes.err}>{error.email}</span>}
                 </div>
 
                 <div className={classes.control}>
                     <label htmlFor='password'>Password:</label>
-                    <input type='password' id ='password' required ref={passwordInputRef}></input>
+                    <input
+                        type='password'
+                        id ='password'
+                        name='password'
+                        value={input.password}
+                        onChange={onInputChange}
+                        onBlur={validateInput}
+                        placeholder='Enter your password...'
+                        required
+                        ref={passwordInputRef}></input>
+                    {error.email && <span className={classes.err}>{error.password}</span>}
                 </div>
 
                 <div className={classes.actions}>
--- frontend/components/login/NewUser.js
+++ frontend/components/login/NewUser.js
@@ -3,6 +3,7 @@ import {useRef, useState} from "react";
 import classes from '../books/NewBookForm.module.css';
 import Card from "../ui/Card";
 import Link from "next/link";
+import {instantiateEmscriptenWasm} from "next/dist/server/lib/squoosh/emscripten-utils";
 
 function NewUser(props) {
     const emailInputRef = useRef();
@@ -28,33 +29,126 @@ function NewUser(props) {
         props.onAddUser(UserData);
     }
 
+    const [input , setInput] = useState({
+        email:'',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const [error , setError] = useState({
+        email:'',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const onInputChange = e => {
+        const {name, value} = e.target;
+        setInput(prev => ({
+        ...prev,
+            [name]: value
+        }));
+        validateInput(e);
+    }
+
+    const validateInput = e => {
+        let {name, value} = e.target;
+        setError(prev => {
+            const stateObj = {...prev, [name]: ""};
+
+            switch (name){
+                case "email":
+                    if(!value){
+                        stateObj[name] = "Please enter a valid student email";
+                    }
+                    break;
+
+                case "password":
+                    if(!value){
+                        stateObj[name] = "Please enter a password";
+                    } else if (input.confirmPassword && value !== input.confirmPassword) {
+                        stateObj["confirmPassword"] = "Password adn Confirm Password are not the same";
+                    } else {
+                        stateObj["confirmPassword"] = input.confirmPassword ? "": error.confirmPassword;
+                    }
+                    break;
+
+                case "confirmPassword":
+                    if(!value){
+                        stateObj[name] = "Please enter a password";
+                    } else if (input.confirmPassword && value !== input.confirmPassword) {
+                        stateObj["confirmPassword"] = "Password and Confirm Password are not the same";
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return stateObj;
+        })
+    }
+
     return (
         <Card>
             <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor='email'>Email:</label>
-                    <input type='text' id ='email' placeholder='Enter your student Email...' required ref={emailInputRef}></input>
+                    <input
+                        type='text'
+                        id ='email'
+                        name = 'email'
+                        value={input.email}
+                        onChange={onInputChange}
+                        onBlur={validateInput}
+                        placeholder='Enter your student Email...'
+                        required
+                        ref={emailInputRef}></input>
+                    {error.email && <span className={classes.err}>{error.email}</span>}
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor='name'>Email:</label>
-                    <input type='text' id ='name' placeholder='Enter your Name...' required ref={nameInputRef}></input>
+                    <label htmlFor='name'>Name:</label>
+                    <input
+                        type='text'
+                        id ='name'
+                        placeholder='Enter your Name...'
+                        ref={nameInputRef}></input>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='password'>Create Password:</label>
-                    <input type='password' id ='password' required ref={passwordInputRef}></input>
+                    <input
+                        type='password'
+                        id ='password'
+                        name='password'
+                        value={input.password}
+                        onChange={onInputChange}
+                        onBlur={validateInput}
+                        placeholder='Enter your password...'
+                        required
+                        ref={passwordInputRef}></input>
+                    {error.email && <span className={classes.err}>{error.password}</span>}
+
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='confirmPassword'>Confirm Password:</label>
-                    <input type='password' id ='confirmPassword' required ref={passwordConfirmInputRef}></input>
-                </div>
+                    <input
+                        type='password'
+                        id ='confirmPassword'
+                        name = 'confirmPassword'
+                        value={input.confirmPassword}
+                        onChange={onInputChange}
+                        onBlur={validateInput}
+                        placeholder='Enter your password again...'
+                        required
+                        ref={passwordConfirmInputRef}></input>
+                    {error.email && <span className={classes.err}>{error.confirmPassword}</span>}
 
+                </div>
                 <div className={classes.actions}>
                     <button>Submit</button>
 
                 </div>
             </form>
             <div className={classes.link}>
-                <div>Already have a User?</div>
+                <div>Already have an Account?</div>
                 <Link href='/login-user'>Login here</Link>
             </div>
         </Card>
