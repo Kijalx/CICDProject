e95c53eceb25ad3e4a2b1d93a10775ff72765212
aleks <alekskijewski@gmail.com>
2023-12-21 21:58:12 +0000

    Creating Events

---
+++ EventDatabase/src/main/java/ie/atu/eventdatabase/EventDatabaseApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.eventdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDatabaseApplication.class, args);
+	}
+
+}
---
+++ EventDatabase/src/test/java/ie/atu/eventdatabase/EventDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.eventdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
