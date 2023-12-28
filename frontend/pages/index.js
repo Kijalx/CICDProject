94c8546ebbdc0a391513b38c8d269b749a0b7b30
sarah mitchell <g00391704@atu.ie>
2023-12-28 16:16:03 +0000

    Added Dropdown in menu

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,9 +3,12 @@ import classes from './main.module.css'
 function HomePage() {
     return <div className={classes.mainDiv}>
         <div className={classes.welcome}>
-            Welcome to the ATU Library
+            Welcome to the Library
             <div className={classes.welcomeLower}>
                 Check out recent events or books added
+            </div>
+            <div className={classes.welcomeLower}>
+                You can also Login to create and add Events and Books
             </div>
         </div>
     </div>
