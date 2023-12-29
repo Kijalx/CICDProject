b3e0be2867faec045f41d7a8371765a1a04fec17
SarahMATU <115370785+SarahMATU@users.noreply.github.com>
2023-12-29 18:49:05 +0000

    Added Dropdown in menu (#15)

--- frontend/components/layout/MainNavigation.js
+++ frontend/components/layout/MainNavigation.js
@@ -1,33 +1,64 @@
-import classes from './MainNavigation.module.css'
-import Link from 'next/link'
-import { useContext } from 'react'
-import GlobalContext from "../../pages/store/globalContext"
-import { useRouter } from 'next/router'
+import classes from './MainNavigation.module.css';
+import Link from 'next/link';
+import { useState } from 'react';
 
 function MainNavigation() {
-  const globalCtx = useContext(GlobalContext)
-  const router = useRouter()
 
-  return (
-    <header className={classes.header}>
-      <Link className={classes.title} href='/'>ATU Library</Link>
-      <nav>
-          <ul>
-            <li>
-                <Link href='/forBooks/new-book'>Add New Book</Link>
-                <Link href='/forBooks/books-open'>All Books</Link>
-            </li>
-          <li>
-              <Link href='/forEvents/new-event'>Add New Event</Link>
-              <Link href='/forEvents/events-open'>All Events</Link>
-          </li>
-            <li>
-              <Link href='/forLogin/login-user'>Login</Link>
-            </li>
-          </ul>
-      </nav>
-    </header>
-  );
+    const [isBooksDropdownOpen, setIsBooksDropdownOpen] = useState(false);
+    const [isEventsDropdownOpen, setIsEventsDropdownOpen] = useState(false);
+
+    const toggleBooksDropdown = () => {
+        setIsBooksDropdownOpen(!isBooksDropdownOpen);
+        setIsEventsDropdownOpen(false);
+    };
+
+    const toggleEventsDropdown = () => {
+        setIsEventsDropdownOpen(!isEventsDropdownOpen);
+        setIsBooksDropdownOpen(false);
+    };
+    const selectItem = (item) => {
+        setIsBooksDropdownOpen(false);
+        setIsEventsDropdownOpen(false);
+    };
+
+    return (
+        <header className={classes.header}>
+            <Link className={classes.title} href='/'>ATU Library</Link>
+            <nav>
+                <ul>
+                    <li className={isBooksDropdownOpen ? classes.dropdownActive : ''}>
+                    <span className={classes.dropdownToggle} onClick={toggleBooksDropdown}>Books</span>
+                        {isBooksDropdownOpen && (
+                            <ul className={classes.dropdownMenu}>
+                                <li onClick={() => selectItem('Add New Book')}>
+                                    <Link href='/forBooks/new-book'>Add New Book</Link>
+                                </li>
+                                <li onClick={() => selectItem('All Books')}>
+                                    <Link href='/forBooks/books-open'>All Books</Link>
+                                </li>
+                            </ul>
+                        )}
+                    </li>
+                    <li className={isEventsDropdownOpen ? classes.dropdownActive : ''}>
+                    <span className={classes.dropdownToggle} onClick={toggleEventsDropdown}>Events</span>
+                        {isEventsDropdownOpen && (
+                            <ul className={classes.dropdownMenu}>
+                                <li onClick={() => selectItem('Add New Event')}>
+                                    <Link href='/forEvents/new-event'>Add New Event</Link>
+                                </li>
+                                <li onClick={() => selectItem('All Events')}>
+                                    <Link href='/forEvents/events-open'>All Events</Link>
+                                </li>
+                            </ul>
+                        )}
+                    </li>
+                    <li>
+                        <Link href='/forLogin/login-user'>Login</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
-export default MainNavigation
+export default MainNavigation;
