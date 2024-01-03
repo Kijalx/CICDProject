f1ffce6ccffe252ee2a29617ab55bcbd15fdb0b2
sarah mitchell <g00391704@atu.ie>
2024-01-03 16:10:42 +0000

    Login add user

--- LoginDatabase/src/main/java/ie/atu/logindatabase/LoginController.java
+++ LoginDatabase/src/main/java/ie/atu/logindatabase/LoginController.java
@@ -4,6 +4,7 @@ import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -16,6 +17,14 @@ public class LoginController {
 
     @PostMapping("/getlogin")
     public Map<String, Object> getPrinter() {return loginService.getLogin();}
+
+    @PostMapping("/addLogin")
+    public Map<String, String> addLogin(@RequestBody Document login) {
+        loginService.addLogin(login);
+        Map<String, String> response = new HashMap<>();
+        response.put("saveLoginResponse", "success");
+        return response;
+    }
 
     @GetMapping("/{id}")
     public Document getLoginById(@PathVariable String id) {return loginService.getLoginById(id);}
